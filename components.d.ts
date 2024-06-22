22d645f25cc80933004e2f34c81383630c4d5afb
ASL <1076976275@qq.com>
2024-06-22 16:24:47 +0800

    feat(element的类型): 补充提交

--- components.d.ts
+++ components.d.ts
@@ -5,16 +5,16 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
 
-declare module 'vue' {
+declare module "vue" {
   export interface GlobalComponents {
-    About: typeof import('./src/components/About.vue')['default']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    Home: typeof import('./src/components/Home.vue')['default']
-    NotFound: typeof import('./src/components/NotFound.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-    User: typeof import('./src/components/User.vue')['default']
+    About: typeof import("./src/components/About.vue")["default"]
+    ElForm: typeof import("element-plus/es")["ElForm"]
+    ElFormItem: typeof import("element-plus/es")["ElFormItem"]
+    ElInput: typeof import("element-plus/es")["ElInput"]
+    Home: typeof import("./src/components/Home.vue")["default"]
+    NotFound: typeof import("./src/components/NotFound.vue")["default"]
+    RouterLink: typeof import("vue-router")["RouterLink"]
+    RouterView: typeof import("vue-router")["RouterView"]
+    User: typeof import("./src/components/User.vue")["default"]
   }
 }
